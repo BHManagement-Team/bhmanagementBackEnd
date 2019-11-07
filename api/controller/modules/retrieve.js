d7df5e57ad8c22d6df8c4a2b09c21c4a989bd812
Christian Gigante <christian.gigante@student.passerellesnumeriques.org>
2019-11-07 15:14:31 +0800

    init

---
+++ api/controller/modules/retrieve.js
@@ -0,0 +1,10 @@
+let retrieveOne = (model,req,res) => {
+// model - from server to database
+// req - from client to server
+// res - from server to client
+}
+let retrieveAll = (model,req,res) => {
+
+};
+
+module.exports = { retrieveOne, retrieveAll }
