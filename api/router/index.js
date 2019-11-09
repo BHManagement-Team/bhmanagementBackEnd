04655bcf044f8f2142764ffc2dee6a72b888ad0b
daynne1726 <dandancaballero99@gmail.com>
2019-11-09 16:21:50 +0800

    with models

---
+++ api/router/index.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+const routes = express.Router();
+const controller = require('../controller/mainController');
+
+routes.route('/retrieve').post((req,res)=>{
+    controller.retrieve(res);
+})
+
+routes.route('/login').post((req,res)=>{
+    controller.login(req, res);
+})
+
+
+module.exports = routes
