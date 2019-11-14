b667b6cce108438f62be8476765c7ec8f1e5267c
cjcabungcag <chillajean.cabungcag@student.passerellesnumeriques.org>
2019-11-14 17:50:09 +0800

    Occupant CRUD added!

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./router')
+const cron = require("node-cron");
+let nodemailer = require("nodemailer");
 
 //DBConfig
 const mongoose = require("mongoose");
@@ -29,6 +31,33 @@ app.use(bodyParser.json({ limit: "20mb" }));
 
 app.use("/bhm", router);
 
+// create mail transporter
+// let transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: "chillajean.cabungcag@student.passerellesnumeriques.org",
+//         pass: ""
+//     }
+// });
+
+// // sending emails at periodic intervals
+// cron.schedule("* * * * *", function () {
+//     console.log("---------------------");
+//     console.log("Running Cron Job");
+//     let mailOptions = {
+//         from: "chillajean.cabungcag@student.passerellesnumeriques.org",
+//         to: "christian.gigante@student.passerellesnumeriques.org",
+//         subject: `Not a GDPR update ;)`,
+//         text: `Hi there, this email was automatically sent by us`
+//     };
+//     transporter.sendMail(mailOptions, function (error, info) {
+//         if (error) {
+//             throw error;
+//         } else {
+//             console.log("Email successfully sent!");
+//         }
+//     });
+// });
 
 app.listen(port, () => {
     console.log("Server is running in port " + port)
