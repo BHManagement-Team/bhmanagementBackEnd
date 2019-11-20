05eabb363ab23d16cba714f5aee227ba7d03549f
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-11-20 15:16:08 +0800

    loginUpdated

--- api/controller/modules/login.js
+++ api/controller/modules/login.js
@@ -3,75 +3,56 @@ const jwt = require("jsonwebtoken");
 const config = require("../../system/config")
 var response = { error: false, success: false }
 
-
 let login = (req, res) => {
-    var usernamei = req.body.username
-    var passwordi = req.body.password
-    User.findOne({ username: usernamei }, (err, data) => {
+    LoginModel.Account.findOne({ username: req.body.username }, (err, result) => {
         if (err) {
-            return res.send(err)
+            response.status = 404
+            response.success = true
+            response.data = result
+            response.auth = false
+            response.message = "Account not found!"
         }
-        if (data != null) {
-            var match = bcrypt.compareSync(passwordi, data.password)
-            if (match) {
-                var acc_token = jwt.sign({ data }, config.secret, { expiresIn: "12h" })
-                return res.send(
-                    response.status = 200,
-                    response.auth = true,
-                    response.data = data,
-                    response.token = acc_token,
-                    response.message = "Successfully Login!"
-                )
-            } else {
-                return res.send(
-                    response.status = 404,
-                    response.auth = false,
-                    response.message = "Incorrect Password!!"
-                )
+        if (result != null) {
+            if (result.password === req.body.password) {
+                var token = jwt.sign({
+                    result
+                }, config.secret, {
+                        expiresIn: 86400 // expires in 24 hours
+                    })
+                response.token = token
+                response.status = 200
+                response.success = true
+                response.data = result
+                response.auth = true
+                response.message = "Successfully Login!"
+            }
+            else {
+                response.token = null
+                response.success = false
+                response.auth = false
+                response.status = 404
+                response.data = null
+                response.message = "Incorrect Password!"
             }
         }
-        return res.send(
-            response.status = 500,
-            response.auth = false,
-            response.message = "Username Not Found!!"
-        )
+        else if (result == null) {
+            response.token = null
+            response.success = false
+            response.auth = false
+            response.status = 404
+            response.data = null
+            response.message = "Username Not Found!"
+        }
     })
+        .catch(err => {
+            if (err) {
+                response.auth = false
+                response.status = 503
+                response.error = true
+                response.data = err
+                response.message = "Service Unavailable!"
+            }
+        });
+    res.send(response);
 }
-
-
-
-// let login = (req, res) => {
-//     LoginModel.Account.findOne({ username: req.body.username }, { password: req.body.password }, (err, result) => {
-//         if (err) {
-//             response.status = 404
-//             response.success = true
-//             response.data = result
-//             response.auth = false
-//             response.message = "Account not found!"
-//         } else {
-
-//             var token = jwt.sign({
-//                 result
-//             }, config.secret, {
-//                     expiresIn: 86400 // expires in 24 hours
-//                 })
-//             response.token = token
-//             response.status = 200
-//             response.success = true
-//             response.data = result
-//             response.auth = true
-//             response.message = "Service running!"
-//         }
-//     })
-//         .catch(err => {
-//             if (err) {
-//                 response.auth = false
-//                 response.status = 503
-//                 response.error = true
-//                 response.data = err
-//                 response.message = "Service Unavailable!"
-//             }
-//         });
-//     res.send(response);
-// }
 module.exports = { login }
