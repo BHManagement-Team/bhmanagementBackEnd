ff06616a1ad21638d53dc2193dfcd99a541f9312
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-12-06 21:50:35 +0800

    almost_DONE_ROOOM

--- api/controller/modules/RoomCRUD/room_routes.js
+++ api/controller/modules/RoomCRUD/room_routes.js
@@ -10,34 +10,71 @@ const modules = {
 }
 //create
 room_routes.route('/createRoom').post((req, res) => {
+    // {
+    //     "token":"xxx",
+    //     "room_name":"baddy",
+    //     "room_floor":"1",
+    //     "room_capacity":3,
+    //     "room_price":100,
+    //     "room_status":true
+    // }
     modules.create.createRoom(req, res)
 })
 
+
 //retrieve
 room_routes.route('/retrieveAllRooms').post((req, res) => {
+    // { "token": "xxx" }
     modules.retrieve.retrieveAllRooms(req, res)
 })
 room_routes.route('/retrieveAllActiveRooms').post((req, res) => {
+    // { "token": "xxx" }
     modules.retrieve.retrieveAllActiveRooms(req, res) //return all room status == true
 })
 
 room_routes.route('/retrieveOneRoom').post((req, res) => {
+    // {"token":"xxx",
+    //  "room_ID": "5dea2bb6f02fd71220674a4b"}
     modules.retrieve.retrieveOneRoom(req, res) //room_Id in req.body
 })
 
 room_routes.route('/retrieveRoomByID/:id').post((req, res) => {
+    //{"token":"xxx"}
+    //req.params.id
     modules.retrieve.retrieveRoombyId(req, res) //room_Id in req.params
 })
+
+
 //update
 room_routes.route('/updateRoom/:id').post((req, res) => {
+    // {
+    //     "token":"xxx",
+    //     "room_name":"helly",
+    //     "room_floor":"1",
+    //     "room_capacity":3,
+    //     "room_price":0,
+    //     "room_status":false
+    // }
     modules.update.updateRoomById(req, res) //room_Id in req.params
 })
 
 room_routes.route('/updateRoom').post((req, res) => {
+    // {
+    //     "token": "xxx",
+    //     "room_ID": "5dea2bc8f02fd71220674a4c",
+    //     "room_name": "lusty",
+    //     "room_floor": "1",
+    //     "room_capacity": 3,
+    //     "room_price": 100,
+    //     "room_status": true
+    // }
     modules.update.updateRoom(req, res) //room_Id in req.body
 })
+
 //remove
 room_routes.route('/deleteRoomByID/:id').post((req, res) => {
+    // {"token":"xxx"}
+    //req.params.id
     modules.delete.removeRoomById(req, res) //room_Id in req.params #just update room status to false
 })
 module.exports = { room_routes }
