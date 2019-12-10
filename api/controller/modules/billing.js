e0855b050fb8cc3bff326dc09dade3fb4d0f089a
2ndyrGroupC <jerome.burce@student.passerellesnumeriques.org>
2019-12-10 12:46:43 +0800

    ALMOST DONE CHAN

--- api/controller/modules/billing.js
+++ api/controller/modules/billing.js
@@ -9,79 +9,39 @@ let response = {
     success: false
 }
 
-//get room amount by room name
-const RoomModel = require('../../model/roomDetails')
-
-// var amount_to_pay = (room_name) => {
-//     try {
-//         RoomModel.Room.findOne({ room_name: room_name }, (err, room) => {
-//             if (err || room == null) {
-//                 return err
-//             } else {
-//                 return room.room_price
-//             }
-//         })
-//     }
-//     catch (err) {
-//         return err
-//     }
-
-// }
-
-let amount_to_pay = async function (room_name) {
-    try {
-        return await RoomModel.Room.findOne({
-            room_name: room_name
-        });
-    } catch (err) {
-        console.log(err);
-
-    }
-}
 
 //try to get the date started of the occupant and compare the current date when the cronjob start
 let billing_cycle = (cron_date) => {
-    try {
-        OccupantModel.Occupant.find({}, (err, data) => {
-            if (err || !data.length) {
 
-                response.error = true
-                response.success = false
-                response.status = 404
-                response.data = err
-                response.message = "No payment found to retrieve!"
-                return res.status(200).send(response)
-            } else {
-                data.forEach(element => {
-                    if (element.date_started == cron_date) {
-                        if (element.date_started.slice(8, 12) == cron_date.slice(8, 12)) {
-
-                            // let amount = amount_to_pay(element.room_name)
-                            console.log(element.occupant_contact, element.occupant_email);
-                            my_emailer.emailer(element.occupant_email, 150)
-                            // my_sms.sms_sender(element.occupant_contact, 1500)
+    OccupantModel.Occupant.find({})
+        .populate('room_ID')
+        .exec(
+            (err, data) => {
+                if (err || !data.length) {
+                    response.error = true
+                    response.success = false
+                    response.status = 404
+                    response.data = err
+                    response.message = "No payment found to retrieve!"
+                    return res.status(200).send(response)
+                } else {
+                    data.forEach(element => {
+                        let amount = element.room_ID.room_price
+                        if (element.date_started == cron_date) {
+                            if (element.date_started.slice(8, 12) == cron_date.slice(8, 12)) {
+                                // let amount = amount_to_pay(element.room_name)
+                                console.log(element.occupant_contact, element.occupant_email, amount);
+                                my_emailer.emailer(element.occupant_email, amount)
+                                // my_sms.sms_sender(element.occupant_contact, 1500)
+                            }
                         }
-                    }
-                });
-                // // response.error = false
-                // // response.success = true
-                // // response.status = 200
-                // response.data = data
-                // // response.message = "Payment Retrieved Successfully!"
-                // return res.status(200).send(response)
-            }
-        })
+                    });
+
+                }
+            })
 
 
-    } catch (err) {
-        // response.error = true
-        // response.success = false
-        // response.data = err
-        // response.status = 503
-        // response.message = "Service Unavailable!"
-        // return res.status(200).send(response)
-        console.log(err);
-    }
+
 }
 module.exports = {
     billing_cycle
