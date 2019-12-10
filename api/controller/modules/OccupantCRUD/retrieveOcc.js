c4c7e40804ec795fdd64065443816e74a32e0a4a
2ndyrGroupC <jerome.burce@student.passerellesnumeriques.org>
2019-12-10 08:33:15 +0800

    Updated occ

--- api/controller/modules/OccupantCRUD/retrieveOcc.js
+++ api/controller/modules/OccupantCRUD/retrieveOcc.js
@@ -3,32 +3,37 @@ let response = {}
 
 let retrieveAllOccupants = (req, res) => {
     if (req.body.token != null) {
-        OccupantModel.Occupant.find({}, (err, data) => {
-            if (err) {
-                response.error = true
-                response.success = false
-                response.status = 404
-                response.data = err
-                response.message = "No Occupant found to retrieve!"
-                return res.status(200).send(response)
-            } else {
-                response.error = false
-                response.success = true
-                response.status = 200
-                response.data = data
-                response.message = "Occupant Retrieved Successfully!"
-                return res.status(200).send(response)
-            }
-        }).catch(err => {
-            if (err) {
-                response.error = true
-                response.success = false
-                response.status = 503
-                response.data = err
-                response.message = "Service Unavailable!"
-                return res.status(200).send(response)
-            }
-        });
+        OccupantModel.Occupant.find({})
+            .populate("room_ID")
+            .exec(
+                (err, data) => {
+                    if (err) {
+                        response.error = true
+                        response.success = false
+                        response.status = 404
+                        response.data = err
+                        response.message = "No Occupant found to retrieve!"
+                        return res.status(200).send(response)
+                    } else {
+                        response.error = false
+                        response.success = true
+                        response.status = 200
+                        response.data = data
+                        response.message = "Occupant Retrieved Successfully!"
+                        return res.status(200).send(response)
+                    }
+                }
+            )
+        // ).catch(err => {
+        //     if (err) {
+        //         response.error = true
+        //         response.success = false
+        //         response.status = 503
+        //         response.data = err
+        //         response.message = "Service Unavailable!"
+        //         return res.status(200).send(response)
+        //     }
+        // });
     } else {
         response.error = true
         response.success = false
